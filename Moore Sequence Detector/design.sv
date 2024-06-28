376bbce5fbd54c2742909e1db6fe7affc3035b4b
Ravi Shah <ravishah6403@gmail.com>
2024-06-28 16:29:24 +0530

    error fix

---
+++ Moore Sequence Detector/design.sv
@@ -0,0 +1,62 @@
+// 1011 Sequence Detector Non-Overlapping
+
+module seq_detect(
+  input clk,
+  input inbits,
+  input reset,
+  output reg detect
+);
+  
+  reg[2:0] state;
+  
+  initial
+    begin
+      state = 3'b000;
+    end
+  
+  always@(posedge clk, posedge reset)
+    begin
+      if (reset) state <= 3'b00;
+      else
+        begin
+          case (state)
+            3'b000:
+              begin
+                if (inbits)	state <= 3'b001;
+                else		state <= 3'b000;
+              end
+            3'b001:
+              begin
+                if (inbits)	state <= 3'b001;
+                else		state <= 3'b010;
+              end
+            3'b010:
+              begin
+                if (inbits)	state <= 3'b011;
+                else		state <= 3'b000;
+              end
+            3'b011:
+              begin
+                if (inbits)	state <= 3'b100;
+                else		state <= 3'b010;
+              end
+            3'b100:
+              begin
+                if (inbits)	state <= 3'b001;
+                else		state <= 3'b000;
+              end
+          endcase
+        end
+    end
+  
+  always@(posedge clk, posedge reset)
+    begin
+      if (reset)
+        detect <= 0;
+      else if (state == 3'b100)
+        detect <= 1;
+      else
+        detect <= 0;
+    end
+  
+endmodule
---
+++ Moore Sequence Detector/testbench.sv
@@ -0,0 +1,55 @@
+`timescale 10ns/100ps
+module testbench;
+  
+  reg clk_tb;
+  reg reset_tb;
+  reg inbits_tb;
+  wire detect_tb;
+  
+  initial
+    begin
+      clk_tb = 0;
+      reset_tb = 0;
+      inbits_tb = 0;
+    end
+  
+  initial
+    begin
+      reset_tb = 1'b1;
+      #5 reset_tb = 1'b0;
+    end
+  
+  always
+    begin
+      #10 clk_tb = !clk_tb;
+    end
+  
+  always
+    begin
+      #20 inbits_tb = 1;
+      #20 inbits_tb = 0;
+      #20 inbits_tb = 1;
+      #20 inbits_tb = 0;
+      #20 inbits_tb = 1;
+      #20 inbits_tb = 1;
+      #20 inbits_tb = 0;
+      #20 inbits_tb = 1;
+      #20 inbits_tb = 0;
+      #20 inbits_tb = 1;
+      #20 inbits_tb = 1;
+      #20 inbits_tb = 1;
+      #20 inbits_tb = 0;
+      #20 inbits_tb = 1;
+      #20 inbits_tb = 1;
+      #20 inbits_tb = 0;
+      #20 inbits_tb = 1;
+      #20 inbits_tb = 0;
+      #20 inbits_tb = 0;
+    end
+  
+  initial #500 $finish;
+  
+  seq_detect seq_detect_inst(.clk(clk_tb), .inbits(inbits_tb), .detect(detect_tb), .reset(reset_tb));
+  
+endmodule
+  
